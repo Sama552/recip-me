c40f5493299fd57fdb31dddbc2ff44527367d639
Sama <sama.naraghi@gmail.com>
2025-02-23 12:40:31 +0200

    minor style changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default async function Home() {
 	return (
 		<main className="flex-1 flex flex-col items-center justify-center min-h-[60vh] px-4">
-			<h1 className="text-4xl font-bold mb-8">Welcome to Your App</h1>
+			<h1 className="text-4xl font-bold mb-8">Welcome to Recip-me</h1>
 			<div className="flex gap-4">
 				<Link
 					href="/sign-in"
